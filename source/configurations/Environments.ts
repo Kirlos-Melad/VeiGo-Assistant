a132eae110361562df74d9d53c136175c97f9b64
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-03-26 20:26:10 +0200

    Add a VERY LONG message upon setting up the communication message
    - Set a funny message to teasse new users
    - Add new env vars for constants

--- source/configurations/Environments.ts
+++ source/configurations/Environments.ts
@@ -11,6 +11,12 @@ const environmentVariables = z.object({
 	DATABASE_CONNECTION: z.string(),
 	DATABASE_DEFAULT_NAME: z.string(),
 	DATABASE_NAME: z.string(),
+	AUTHOR_PROFILE_URL: z.string(),
+	AUTHOR_IMAGE_URL: z.string(),
+	AUTHOR_NAME: z.string(),
+	AUTHOR_NICKNAME: z.string(),
+	AUTHOR_SIGNATURE: z.string(),
+	BOT_NAME: z.string(),
 });
 
 export default environmentVariables.parse(process.env);
